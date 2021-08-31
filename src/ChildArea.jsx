bec727cfb1cba39546d3a6d8ae45092268cab321
Gavriil84 <79692322+Gavriil84@users.noreply.github.com>
2021-08-31 15:43:36 -0700

    used memo and useCallback

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,18 +1,27 @@
+import { memo } from "react";
+
 const style = {
   width: "100%",
   height: "200px",
   backgroundColor: "khaki"
 };
 
-export const ChildArea = (props) => {
-  const { open } = props;
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+  console.log("ChildArea was rendered!!");
+
+  const data = [...Array(100).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
   return (
     <>
       {open ? (
         <div style={style}>
           <p>Child Component</p>
+          <button onClick={onClickClose}>Close</button>
         </div>
       ) : null}
     </>
   );
-};
+});
